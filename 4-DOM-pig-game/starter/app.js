f1cf3c12299b27794c118dd96109cce132bccccf
kenkuts <kennethchoi19@gmail.com>
2020-02-23 16:10:14 -0600

    made dice game using DOM maipulation in JS

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -8,3 +8,85 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+// Pass the pigs:
+
+var scores = [0,0];
+var roundScores = 0; 
+var activePlayer = 0; 
+
+//document.querySelector('#current-' + activePlayer).innerHTML = '';
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+
+    // Generate Random Number
+    var dice = Math.floor(Math.random() * 6 + 1);
+
+    // Disply the dice
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice +'.png';
+    
+    // Game logic
+    if (dice === 1) {
+        // display new score 
+        roundScores = 0;
+        document.querySelector('#current-' + activePlayer).textContent = roundScores;
+        
+        // remove the active to old player and change to the other player
+        document.querySelector('.player-' + activePlayer + '-panel').classList.toggle('active');
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+
+        // add active to the new player
+        document.querySelector('.player-' + activePlayer + '-panel').classList.toggle('active');
+        
+        // dice display is back to none to signify new players turn
+        document.querySelector('.dice').style.display = 'none';
+    } else {
+        roundScores += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScores; 
+    }
+
+
+    // generate a random number for whoevers turn it is
+    // if the number is 1 then reset the accumulated score then switch to next playey 
+
+    // else keep rolling
+    
+    // until 
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
